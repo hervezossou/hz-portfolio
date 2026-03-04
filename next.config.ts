98f0c8ffb119f1e80eddce988b27e170c71e02b2
Hervé Zossou <hervezossou8@gmail.com>
2026-03-04 10:05:39 +0100

    perf: optimize icon imports and clean redundant compiler plugins

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
+    experimental: {
+        optimizePackageImports: [
+            "@hugeicons/react",
+            "@hugeicons/core-free-icons",
+        ],
+    },
 };
 
 export default nextConfig;
